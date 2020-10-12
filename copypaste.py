b612881daab00a78134cbe4e385cb23d3b8e4d91
Sampo Vartiala <sampo.vartiala@gmail.com>
2020-10-12 19:07:19 +0300

    add stuff

---
+++ copypaste.py
@@ -0,0 +1,10 @@
+# Copypaste stuff to telegram
+import time
+keyboard.send_keys("<ctrl>+c")
+
+window.activate("Telegram")
+keyboard.send_keys("\"<ctrl>+v")
+time.sleep(0.1)
+keyboard.send_keys("\" ")
+
+#keyboard.send_keys("\""+text+"\" ")
---
+++ ff-disable-ctrlq.py
@@ -0,0 +1,1 @@
+# Set this to run on ctrl+q to disable it
