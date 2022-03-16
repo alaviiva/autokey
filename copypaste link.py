87a1d210352804f36830ea59d9236f26c13389d6
Sampo Vartiala <sampo.vartiala@gmail.com>
2022-03-16 14:11:29 +0200

    fix ff link

--- copypaste link.py
+++ copypaste link.py
@@ -3,7 +3,8 @@ import time
 
 #window.activate("Firefox")
 #if window.wait_for_focus(".*Firefox.*", 0):
-if window.get_active_class().endswith("Firefox"):
+#dialog.info_dialog(message=window.get_active_class())
+if window.get_active_class().endswith("firefox"):
   keyboard.send_keys("<ctrl>+l")
   time.sleep(0.1)
   keyboard.send_keys("<ctrl>+c")
